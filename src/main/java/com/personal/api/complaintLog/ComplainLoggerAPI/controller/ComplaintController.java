4f9d405dbf9bca82f821bf0db7922a0531675699
shivanijadhav1797 <shivanijadhav1797@gmail.com>
2021-01-13 20:26:09 +0530

    Added @CrossOrigin

--- src/main/java/com/personal/api/complaintLog/ComplainLoggerAPI/controller/ComplaintController.java
+++ src/main/java/com/personal/api/complaintLog/ComplainLoggerAPI/controller/ComplaintController.java
@@ -3,6 +3,7 @@ package com.personal.api.complaintLog.ComplainLoggerAPI.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.personal.api.complaintLog.ComplainLoggerAPI.bean.ResponseBean;
@@ -18,6 +18,7 @@ import com.personal.api.complaintLog.ComplainLoggerAPI.model.Complaints;
 import com.personal.api.complaintLog.ComplainLoggerAPI.service.ComplaintService;
 import com.personal.api.complaintLog.ComplainLoggerAPI.util.Constants;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/complaints")
 public class ComplaintController {
